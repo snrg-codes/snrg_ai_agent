f0291f270b7db45ac3d65ac7b7565ecc2b439fdd
khamraevs <210115349+snrgtemp@users.noreply.github.com>
2025-05-31 10:16:29 +0500

    class Generate

--- functions.py
+++ functions.py
@@ -2,86 +2,51 @@ import base64
 import os
 from google import genai
 from google.genai import types
+                              
+from config import GEMINI_API_TOKEN                 
 
-# from dotenv import load_dotenv                    #
-# load_dotenv()                                     #
-from config import GEMINI_API_TOKEN                 #
-
-with open("main.json", "r") as f:
-    data = f.read()
-
-def generate(savol):
-    client = genai.Client(
-        # api_key=os.getenv("GEMINI_API_TOKEN"),
-        api_key=GEMINI_API_TOKEN,                   #
-    )
-
-    model = "gemini-2.0-flash-lite"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text=f"""{savol}"""),
-            ],
-        ),
-    ]
-    generate_content_config = types.GenerateContentConfig(
-        response_mime_type="text/plain",
-        system_instruction=[
-            types.Part.from_text(text=f"""tasavvur qil sen sotuv menedjerisan
-            {data} - ma'lumotlaridan foydalan
-            va savollarga javob ber. agar ushbu mavzuda qo'shimcha ma'lumot
-            bera olsang ma'lumotni faqat tanishish maqsadida taqdim 
-            etishing mumkin"""),
-        ],
-    )
-
-    javob =  client.models.generate_content(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
+class Generate:
+    def __init__(self, GEMINI_API_TOKEN, data):
+        self.client = genai.Client(
+            api_key=GEMINI_API_TOKEN,
         )
-    return javob.text
-
-with open("ponchiki_namangan.json", "r") as f:
-    ponchiki_namangan = f.read()
-
-def generate_ponchiki_namangan(savol):
-    client = genai.Client(
-        # api_key=os.getenv("GEMINI_API_TOKEN"),
-        api_key=GEMINI_API_TOKEN,                   #
-    )
-
-    model = "gemini-2.0-flash-lite"
-    contents = [
-        types.Content(
-            role="user",
-            parts=[
-                types.Part.from_text(text=f"""{savol}"""),
+        self.model = "gemini-2.0-flash-lite"
+        self.data = data
+    
+        self.generate_content_config = types.GenerateContentConfig(
+            response_mime_type="text/plain",
+            system_instruction=[
+                types.Part.from_text(text=f"""tasavvur qil sen sotuv menedjerisan
+                {self.data} - ma'lumotlaridan foydalan
+                va savollarga javob ber. agar ushbu mavzuda qo'shimcha ma'lumot
+                bera olsang ma'lumotni faqat tanishish maqsadida taqdim 
+                etishing mumkin"""),
             ],
-        ),
-    ]
-    generate_content_config = types.GenerateContentConfig(
-        response_mime_type="text/plain",
-        system_instruction=[
-            types.Part.from_text(text=f"""tasavvur qil sen sotuv menedjerisan
-            {ponchiki_namangan} - ma'lumotlaridan foydalan
-            va savollarga javob ber. agar ushbu mavzuda qo'shimcha ma'lumot
-            bera olsang ma'lumotni faqat tanishish maqsadida taqdim 
-            etishing mumkin"""),
-        ],
-    )
-
-    javob =  client.models.generate_content(
-        model=model,
-        contents=contents,
-        config=generate_content_config,
         )
-    return javob.text
+    
+    def generate(self, savol):
+        self.contents = [
+            types.Content(
+                role="user",
+                parts=[
+                    types.Part.from_text(text=f"""{savol}"""),
+                ],
+            ),
+        ]
+        javob =  self.client.models.generate_content(
+                model=self.model,
+                contents=self.contents,
+                config=self.generate_content_config,
+        )
+        return javob.text
 
+if __name__ == "__main__":  
+     
+    with open("namdu.json", 'r', encoding='utf-8') as f:
+        data = f.read()
 
-
-if __name__ == "__main__":
-    a = generate_ponchiki_namangan("Qanday ponchik turlari mavjud?")
-    print(a)
-
+    namdu = Generate(GEMINI_API_TOKEN, data)
+    savol = "Kimyoni o'rganib qaysi yo'nalishga kirsam bo'ladi"
+    javob = namdu.generate(savol)
+    print(javob)
+       
